bf5f5b7b71fc1fdfcf79d25047672fa776b7378f
shivamtr21 <shivamtripathi26297@gmail.com>
2023-07-13 16:17:10 +0530

    [Shivam Tripathi] :Added Employee payroll UC3

--- Employee_Payroll.sql
+++ Employee_Payroll.sql
@@ -14,4 +14,13 @@ create table employee_payroll
  StartDate Date NOT NULL
 )
 
+
+ 
+ /* Inserting items */
+
+insert into employee_payroll values('Sanjam', 50000, '2023-04-21'),
+                                   ('Shivam', 20000, '2023-04-21'),
+								   ('Divya', 70000, '2023-04-21'),
+								   ('Aakanksha', 50000, '2023-04-21')
+
 select * from employee_payroll
