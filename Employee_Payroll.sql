ad8e22689efab2691d82cac802804419024d970a
shivamtr21 <shivamtripathi26297@gmail.com>
2023-07-13 16:12:55 +0530

    [Shivam Tripathi] :Added Employee payroll UC2

--- Employee_Payroll.sql
+++ Employee_Payroll.sql
@@ -2,3 +2,16 @@ CREATE DATABASE Payroll_Service  /*   Create database */
 
 
 USE Payroll_Service
+
+
+ 
+
+create table employee_payroll
+(
+ Id int primary key identity,
+ Name varchar(40) NOT NULL,
+ Salary int,
+ StartDate Date NOT NULL
+)
+
+select * from employee_payroll
