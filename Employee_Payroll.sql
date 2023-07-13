ccf4d9aee8586fea14a56650498326ca6d2a7fd0
shivamtr21 <shivamtripathi26297@gmail.com>
2023-07-13 16:25:24 +0530

    [Shivam Tripathi] :Added Employee payroll UC5

--- Employee_Payroll.sql
+++ Employee_Payroll.sql
@@ -25,3 +25,14 @@ insert into employee_payroll values('Sanjam', 50000, '2023-04-21'),
 
 								   /* Retrive data  */
 select * from employee_payroll
+
+select Salary from employee_payroll where id = 3   /* salary of particular employee */
+
+select * from employee_payroll where StartDate BETWEEN CAST('2023-07-11' AS DATE) AND GETDATE() /* employee deatil b/w date range */
+
+/* upadte date */
+
+update employee_payroll set StartDate = '2023-07-12'  where id = 1   
+update employee_payroll set StartDate = '2023-07-11'  where id = 2
+update employee_payroll set StartDate = '2023-07-13'  where id = 3
+update employee_payroll set StartDate = '2023-07-14'  where id = 4
